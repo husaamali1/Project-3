f4cf38b1cb6d1bb94ade48a1aa228d75e7bb677a
Husaam ali <husaam.ha1@gmail.com>
2024-02-02 11:53:52 +0000

    deployment to heroku

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import router from './config/routes.js'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 
 const app = express()
 
@@ -15,6 +22,16 @@ app.use((req, res, next) => {
 
 // ? Endpoints
 app.use('/api', router)
+
+// The below line is the router, this should already be present and doesn't need to be added. Add everything below it.
+app.use('/api', router) 
+
+// ** New lines **
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 async function startServer(){
   try {
